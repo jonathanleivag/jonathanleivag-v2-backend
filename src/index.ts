c78ed957235747d50692dfa919a8bd8c506e579d
Jonathanleivag <email@jonathanleivag.cl>
2025-01-19 16:40:16 -0300

    Add CORS support to the Express app, update package dependencies, and configure CORS options for enhanced security and functionality

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,21 @@ import express from 'express'
 import { getEnv } from './utils/env.util'
 import heroRouter from './router/hero.router'
 import { closeDB, connectDB } from './database'
+import cors from 'cors'
+
+const PORT = getEnv('PORT')
 
 const app = express()
 
-const PORT = getEnv('PORT')
+const corsOptions = {
+  origin: getEnv('URI'),
+  optionsSuccessStatus: 200,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With', 'X-CSRF-Token', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Headers']
+}
+
+app.use(cors(corsOptions))
+app.use(express.json({ limit: '1mb' }))
 
 app.get('/', (_req, res) => {
   res.redirect(getEnv('URI'))
