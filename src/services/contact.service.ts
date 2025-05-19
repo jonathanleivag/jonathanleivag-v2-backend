fc6d87ed3e24d5b3e1406504e1cb3dfa57614ba9
Jonathanleivag <email@jonathanleivag.cl>
2025-05-19 18:34:39 -0400

    Refactor email service and update workspace configurations
    Refactored email-related functions for improved readability, formatting, and added a delay mechanism using `sleep`. Updated `.idea/workspace.xml` to include new HTTP client configurations and cleaned up formatting issues.

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -9,7 +9,10 @@ import { WithId } from 'mongodb'
 export const getContact = async (): Promise<Res<Array<WithId<Contact>>>> => {
   try {
     console.log('🚀 ~ getContact ~ getContact')
-    const contacts = await getDB().collection<Contact>(COLLECTION.CONTACTS).find().toArray()
+    const contacts = await getDB()
+      .collection<Contact>(COLLECTION.CONTACTS)
+      .find()
+      .toArray()
     if (contacts == null) {
       throw new Error('Contact not found')
     }
@@ -38,24 +41,31 @@ export const getContact = async (): Promise<Res<Array<WithId<Contact>>>> => {
   }
 }
 
-const insertEmail = async (resend: Resend, data: { id: string }): Promise<void> => {
+const insertEmail = async (
+  resend: Resend,
+  data: { id: string }
+): Promise<void> => {
   try {
     console.log('🚀 ~ sendEmail')
-    const { data: retrieve, error: errorRetrieve } = await resend.emails.get(data.id)
+    const { data: retrieve, error: errorRetrieve } = await resend.emails.get(
+      data.id
+    )
     if (errorRetrieve !== null) {
       console.error('🚀 ~ sendEmail ~ errorRetrieve:', errorRetrieve)
     }
 
     if (retrieve !== null) {
       const emailData = retrieve as unknown as Data | null
-      await getDB().collection<ContactOmit>(COLLECTION.CONTACTS).insertOne({
-        email: {
-          data: emailData,
-          error: errorRetrieve
-        },
-        createdAt: new Date(),
-        updatedAt: new Date()
-      })
+      await getDB()
+        .collection<ContactOmit>(COLLECTION.CONTACTS)
+        .insertOne({
+          email: {
+            data: emailData,
+            error: errorRetrieve
+          },
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
     }
   } catch (error) {
     if (error instanceof Error) {
@@ -64,7 +74,12 @@ const insertEmail = async (resend: Resend, data: { id: string }): Promise<void> 
   }
 }
 
-const email = async (name: string, content: string, options: CreateEmailOptions, print: boolean = false): Promise<void> => {
+const email = async (
+  name: string,
+  content: string,
+  options: CreateEmailOptions,
+  print: boolean = false
+): Promise<void> => {
   console.log('🚀 ~ email ~ email')
   try {
     const resend = new Resend(getEnv(ENV.KEY_RESEND))
@@ -76,7 +91,9 @@ const email = async (name: string, content: string, options: CreateEmailOptions,
       from: options.from,
       to: options.to,
       subject: options.subject,
-      html: options.html.replace('<<content>>', content).replace('<<name>>', name)
+      html: options.html
+        .replace('<<content>>', content)
+        .replace('<<name>>', name)
     })
 
     if (error !== null) {
@@ -94,12 +111,39 @@ const email = async (name: string, content: string, options: CreateEmailOptions,
   }
 }
 
-export const sendEmail = async (name: string, content: string, emailTO: string): Promise<ResWhitOutData> => {
+const sleep = async (ms: number): Promise<void> =>
+  await new Promise((resolve) => setTimeout(resolve, ms))
+
+export const sendEmail = async (
+  name: string,
+  content: string,
+  emailTO: string
+): Promise<ResWhitOutData> => {
   console.log('🚀 ~ sendEmail ~ sendEmail')
   try {
-    await email(getEnv(ENV.NAME), content, { from: getEnv(ENV.FROM), to: getEnv(ENV.TO), subject: getEnv(ENV.SUBJECT), html }, true)
-
-    await email(name, getEnv(ENV.CONTENT_RESEND), { from: getEnv(ENV.FROM), to: emailTO, subject: getEnv(ENV.SUBJECT), html }, false)
+    await email(
+      getEnv(ENV.NAME),
+      content,
+      {
+        from: getEnv(ENV.FROM),
+        to: getEnv(ENV.TO),
+        subject: getEnv(ENV.SUBJECT),
+        html
+      },
+      true
+    )
+    await sleep(1000)
+    await email(
+      name,
+      getEnv(ENV.CONTENT_RESEND),
+      {
+        from: getEnv(ENV.FROM),
+        to: emailTO,
+        subject: getEnv(ENV.SUBJECT),
+        html
+      },
+      false
+    )
 
     return {
       error: null,
